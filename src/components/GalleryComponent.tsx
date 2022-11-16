03c6f24d7b757f27ec16e811f19fb833a65dfb8d
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2022-11-16 17:14:07 +0100

    Add functions

--- src/components/GalleryComponent.tsx
+++ src/components/GalleryComponent.tsx
@@ -1,3 +1,12 @@
-export default function GalleryComponent() {
+import {Character} from "../model/Character";
 
+type GalleryComponentProps = {
+    characters:Character[]
+}
+
+export default function GalleryComponent(props:GalleryComponentProps ) {
+    const characterComponents = props.characters.map((character:Character) => {
+        return <C
+
+    })
 }
