9baa629acc7937f954f7b03efe07a0432becc153
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2022-11-17 12:06:17 +0100

    Add searchfunction

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GalleryComponent from "./components/GalleryComponent";
 import {CHARACTERS} from "./components/characterRickAndMorty";
 
 function App() {
-  return (
-    <div className="App">
-      <GalleryComponent characters={CHARACTERS}></GalleryComponent>
-    </div>
-  );
+    const [search, setSearch] = useState('');
+
+
+    const suchen = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+    }
+
+    const filtered = () => {
+        return search !== '' ? CHARACTERS.filter(character => character.name.toUpperCase().includes(search.toUpperCase())) : CHARACTERS;
+    }
+
+    return (
+        <div className="App">
+            <input type="text" onChange={suchen}/>
+            <GalleryComponent characters={filtered()}></GalleryComponent>
+        </div>
+    );
 }
 
 export default App;
